03cf7e9e9a359b450427d42754ca440df27ea3e9
Laura Davies <drljdavies@gmail.com>
2020-07-01 11:55:21 -1000

    Completed rps walk through videos

---
+++ backend/RB120_RB129/rock_paper_scissors/rps_walk_through_design_choice_2.rb
@@ -0,0 +1,136 @@
+class Move
+  VALUES = ['rock', 'paper', 'scissors']
+
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def >(other_move)
+    (rock? &&  other_move.scissors?) ||
+    (paper? && other_move.rock?) ||
+    (scissors? && other_move.paper?)
+  end
+
+  def <(other_move)
+    (rock? && other_move.paper?) ||
+    (paper? && other_move.scissors?) ||
+    (scissors? && other_move.rock?)
+  end
+
+  def to_s
+    @value
+  end
+end
+
+class Player
+  attr_accessor :move, :name
+
+  def initialize
+    set_name
+  end
+ end
+
+class Human < Player
+  def set_name
+    n = ""
+    loop do
+      puts "Please enter your name:"
+      n = gets.chomp
+      break unless n.empty?
+      puts "Try again. Please enter your name:"
+    end
+    self.name = n
+  end
+
+  def choose
+    choice = nil
+    loop do
+      puts "Do you choose rock, paper or scissors?"
+      choice = gets.chomp
+      break if Move::VALUES.include?(choice)
+      puts "Invalid choice, enter rock, paper or scissors:"
+    end
+    self.move = Move.new(choice)
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = 'Acorn'
+  end
+
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
+  end
+end
+
+class RPSGame
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors."
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors!"
+  end
+
+  def display_moves
+    puts "#{human.name} chose #{human.move}."
+    puts "#{computer.name} chose #{computer.move}."
+  end
+
+  def display_winner
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? Enter yes or no:"
+      answer = gets.chomp
+      break if answer == 'yes' || answer == 'no'
+      puts "Incorrect response, enter yes or no:"
+    end
+
+    if answer == 'yes'
+      RPSGame.new.play # not sure if this is good practise? Instantiating a new object within an object?  Probably not..
+    else
+      display_goodbye_message
+    end
+  end
+
+  def play
+    display_welcome_message
+    human.choose
+    computer.choose
+    display_moves
+    display_winner
+    play_again?
+  end
+end
+
+RPSGame.new.play
